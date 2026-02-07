2ec3526328c8051051732f0aba358eb8f205d755
theharishpuri <theharishpuri@gmail.com>
2026-02-07 15:48:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// Register Service Worker
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("sw.js")
+      .then(() => console.log("✅ Service Worker Registered"))
+      .catch(err => console.log("SW error:", err));
+  });
+}
 
